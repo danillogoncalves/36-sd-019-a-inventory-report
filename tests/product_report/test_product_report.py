71b0e33763d9d874553efa52e43c21ab6b44002e
danillogoncalves <danillogoncalves001@gmail.com>
2022-10-30 17:29:42 -0300

    test: :test_tube: Requisito 2 feito.

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_mock = {
+        "id": 26,
+        "nome_do_produto": "flour",
+        "nome_da_empresa": "food company",
+        "data_de_fabricacao": "10-31-2022",
+        "data_de_validade": "01-31-2023",
+        "numero_de_serie": "D1D4 55FF U8I7 E7W8 9",
+        "instrucoes_de_armazenamento": "sheltered from light",
+    }
+
+    product = Product(**product_mock)
+
+    expect = (
+        "O produto flour fabricado em 10-31-2022"
+        " por food company com validade"
+        " até 01-31-2023"
+        " precisa ser armazenado sheltered from light."
+    )
+
+    assert str(product) == expect
